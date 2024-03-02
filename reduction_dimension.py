36d9b62654ae49fa86be25c8970edce9fdf0ddf0
ArnaudFB <favrebarnaud@gmail.com>
2024-03-02 23:47:20 +0100

    Test avec les masked array

--- reduction_dimension.py
+++ reduction_dimension.py
@@ -20,35 +20,16 @@ target = ['burn-up', 'bore', 'P_rel', 'T_entree', 'insertion_barres'] #Variables
 x = nappe_data.loc[:, features]
 y = nappe_data.loc[:, target]
 
-# Coordonnées d'assemblage non utiles
-coord_assemblages_inutiles = [ [0,0], [0,1], [0,2], [0,3], [0,4], [0,5],
-                               [1,0], [1,1],               [1,4], [1,5],
-                               [2,0],                             [2,5],
-                               [3,0],                             [3,5],
-                               [4,0], [4,1],               [4,4], [4,5],
-                               [5,0], [5,1], [5,2], [5,3], [5,4], [5,5],
-                               ]
-
 # Traitement des données nulles
-x.values.reshape(1080, 102, 102, 14)
-print(x.values[1].reshape(17*6, 17*6, 14).shape)
+x = np.ma.masked_equal(x, 0)
 
-#nappe_data_filtered = nappe_data.loc[:, (nappe_data != 0).all(axis=0)]
-#nappe_data_filtered = nappe_data_filtered.loc[~(nappe_data_filtered[list(range(N - 5))] == 0).all(axis=1)]
-
-# Variable target et features
-features = list(range(N - 5)) #Variables du réacteur
-target = ['burn-up', 'bore', 'P_rel', 'T_entree', 'insertion_barres'] #Variables que l'on cherche à expliquer
-
-# Attribution des values de nos variables pour les standardiser
-x = nappe_data.loc[:, features]
-y = nappe_data.loc[:, target]
+# Standardisation des données
 x = StandardScaler().fit_transform(x)
 
 # Statistiques decriptives des données
 print([nappe_data[target].mean(), nappe_data[target].quantile([0.25, 0.5, 0.75]), nappe_data[target].max(), nappe_data[target].min()])
 # print(x)
-"""
+
 # Réalisation de l'ACP
 pca = PCA(n_components=3)
 nappe_data_pca = pca.fit_transform(x)
@@ -118,4 +99,4 @@ ax.axhline(0, color='black',linewidth=0.5)
 ax.axvline(0, color='black',linewidth=0.5)
 ax.set_title('Correlation Circle')
 
-plt.show()"""
+plt.show()
